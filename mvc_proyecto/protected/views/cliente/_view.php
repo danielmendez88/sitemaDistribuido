34f7adc3b1af543743d44ee6d403badfc61905a6
mgl2809 <marcos_glez>
2015-09-07 00:26:05 -0500

    Clientes
    Actualización clientes

---
+++ mvc_proyecto/protected/views/cliente/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this ClienteController */
+/* @var $data Cliente */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nombre')); ?>:</b>
+	<?php echo CHtml::encode($data->nombre); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('apellido')); ?>:</b>
+	<?php echo CHtml::encode($data->apellido); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('razonsocial')); ?>:</b>
+	<?php echo CHtml::encode($data->razonsocial); ?>
+	<br />
+
+
+</div>
--- mvc_proyecto/protected/views/layouts/main.php
+++ mvc_proyecto/protected/views/layouts/main.php
@@ -32,6 +32,7 @@
 				array('label'=>'Inicio', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Clientes', 'url'=>array('/cliente/cliente')),
 				array('label'=>'Productos', 'url'=>array('/productos/index')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
