a0344d59274c3faeb3c8da4bf3ec54c939490deb
danielmendez88 <daniboymendez@gmail.com>
2015-09-08 01:37:04 -0500

    actualización del fichero de la base de datos

--- proyectomvc.sql
+++ proyectomvc.sql
@@ -26,27 +26,45 @@ CREATE TABLE `cliente` (
   `apellido` varchar(50) DEFAULT NULL,
   `razonsocial` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `cliente` */
 
-/*Table structure for table `producto_por_venta` */
+insert  into `cliente`(`id`,`nombre`,`apellido`,`razonsocial`) values (1,'Daniel','Méndez Cruz','21832ijsdlkjdkls'),(2,'Manuel','Cruz Gutiérrez','21232jdlkfdjs'),(3,'María del Carmen','Cruz Torres','CRTMC12403');
 
-DROP TABLE IF EXISTS `producto_por_venta`;
+/*Table structure for table `cliente_compra` */
 
-CREATE TABLE `producto_por_venta` (
-  `ventas_idventas` int(11) NOT NULL,
+DROP TABLE IF EXISTS `cliente_compra`;
+
+CREATE TABLE `cliente_compra` (
+  `compra_idventa` int(11) NOT NULL,
   `productos_idproductos` int(11) NOT NULL,
   `cantidad` int(11) DEFAULT NULL,
-  `total` float DEFAULT NULL,
-  PRIMARY KEY (`ventas_idventas`,`productos_idproductos`),
-  KEY `fk_ventas_has_productos_productos1_idx` (`productos_idproductos`),
-  KEY `fk_ventas_has_productos_ventas1_idx` (`ventas_idventas`),
-  CONSTRAINT `fk_ventas_has_productos_productos1` FOREIGN KEY (`productos_idproductos`) REFERENCES `productos` (`idproductos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ventas_has_productos_ventas1` FOREIGN KEY (`ventas_idventas`) REFERENCES `ventas` (`idventas`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `subtotal` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`compra_idventa`,`productos_idproductos`),
+  KEY `fk_compra_has_productos_productos1_idx` (`productos_idproductos`),
+  KEY `fk_compra_has_productos_compra1_idx` (`compra_idventa`),
+  CONSTRAINT `fk_compra_has_productos_compra1` FOREIGN KEY (`compra_idventa`) REFERENCES `compra` (`idventa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_compra_has_productos_productos1` FOREIGN KEY (`productos_idproductos`) REFERENCES `productos` (`idproductos`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
-/*Data for the table `producto_por_venta` */
+/*Data for the table `cliente_compra` */
+
+/*Table structure for table `compra` */
+
+DROP TABLE IF EXISTS `compra`;
+
+CREATE TABLE `compra` (
+  `idventa` int(11) NOT NULL,
+  `total` decimal(5,2) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `cliente_id` int(10) NOT NULL,
+  PRIMARY KEY (`idventa`),
+  KEY `fk_compra_cliente1_idx` (`cliente_id`),
+  CONSTRAINT `fk_compra_cliente1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `compra` */
 
 /*Table structure for table `productos` */
 
@@ -60,29 +78,11 @@ CREATE TABLE `productos` (
   `codigo` varchar(45) DEFAULT NULL,
   `descripcion` text,
   PRIMARY KEY (`idproductos`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Data for the table `productos` */
 
-insert  into `productos`(`idproductos`,`nombreproducto`,`precio`,`existencia`,`codigo`,`descripcion`) values (1,'producto1','52.10',122,'01923847','Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.\r\n\r\nDonec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'),(2,'producto2','12.10',22,'01923821','pCras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.\r\n\r\nDonec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'),(3,'producto3','10.40',12,'019220394','Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.\r\n\r\nDonec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'),(4,'producto4','5.10',30,'01922347','Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.\r\n\r\nDonec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
-
-/*Table structure for table `ventas` */
-
-DROP TABLE IF EXISTS `ventas`;
-
-CREATE TABLE `ventas` (
-  `idventas` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date DEFAULT NULL,
-  `subtotal` float NOT NULL,
-  `iva` float NOT NULL,
-  `total` float NOT NULL,
-  `cliente_id` int(10) NOT NULL,
-  PRIMARY KEY (`idventas`,`cliente_id`),
-  KEY `fk_ventas_cliente_idx` (`cliente_id`),
-  CONSTRAINT `fk_ventas_cliente` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `ventas` */
+insert  into `productos`(`idproductos`,`nombreproducto`,`precio`,`existencia`,`codigo`,`descripcion`) values (1,'producto1','52.10',10,'H1N10','esto describe el producto 1'),(2,'producto2','10.20',40,'H1N11','descripción del producto 2'),(3,'producto3','40.20',70,'H1N12','descripción del producto 3'),(4,'producto4','20.20',50,'H1N13','descripción del producto 4'),(5,'nuevo producto','23.50',123,'HEROD20129',NULL),(6,'producto4','234.00',23,'REAL12039',NULL),(9,'producto10','23.50',120,'AHE1920','esto es otra descripción de prueba espero que funcione al 100%');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
